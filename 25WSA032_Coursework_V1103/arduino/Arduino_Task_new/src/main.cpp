327d8e24eed08cd0593920ac59feef482d81f3d3
536539lboro-lab <536539.lboro@gmail.com>
2026-03-17 10:21:10 +0000

    # Change for arduino code

--- 25WSA032_Coursework_V1103/arduino/Arduino_Task_new/src/main.cpp
+++ 25WSA032_Coursework_V1103/arduino/Arduino_Task_new/src/main.cpp
@@ -4,8 +4,11 @@
 int myFunction(int, int);
 
 void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  Serial.print(result);
+
 }
 
 void loop() {
